a268c96217c6bee08f692d399ca68f6840e3c464
tifosiBarca <g.owenhe@gmail.com>
2022-05-06 14:23:30 -0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'tamu-polo-club.herokuapp.com', # UPDATE THIS VALUE WITH YOUR OWN APP
+    :domain         => 'https://mighty-beach-14261.herokuapp.com/', # UPDATE THIS VALUE WITH YOUR OWN APP
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
